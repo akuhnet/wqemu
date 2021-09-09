bcc140793b92a3a776790b64bdc884e5385b8ce4
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-09 20:52:42 +0700

    Create chrome_python.py

---
+++ chrome_python.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.youtube.com/channel/UC-3M3r3D3UwfpzYTjPYmVfw")
