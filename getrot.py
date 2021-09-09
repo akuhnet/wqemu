7d9863972774f43692fd891af842194c4e42a536
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-09 22:06:37 +0700

    Create getrot.py

---
+++ getrot.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='1b9rn53R_gQoQzsoqL0Uq731bhLfGmq4K',
+                                    dest_path='./akuh.zip',
+                                    unzip=True)
