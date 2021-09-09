1fd89a667e42913d60d4f3b5ef9d4653329f412f
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-09 23:15:18 +0700

    Update su.sh

--- su.sh
+++ su.sh
@@ -6,8 +6,8 @@ wget https://raw.githubusercontent.com/akuhnet/wqemu/main/getrot.py
 python getrot.py
 unzip root.zip
 tar -xvf root.tar.xz
-./dist/proot -S . /bin/bash
 echo "===================================="
 echo "Keep supporting akuh.net, thank you"
 echo "You Got Root access now"
 echo "===================================="
+./dist/proot -S . /bin/bash
