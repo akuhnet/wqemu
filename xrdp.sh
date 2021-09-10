c3f37f1eecd743e66a1d81743b12847d71087fc8
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-10 19:17:36 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,6 +1,9 @@
-echo "===================================="
-echo "More  Free VPS akuh.net"
-echo "===================================="
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP
+./ngrok tcp 3389 &>/dev/null &
 sudo apt install -y xfce4 xfce4-goodies
 sudo apt-get install -y firefox
 sudo apt-get install -y xrdp
@@ -9,11 +12,8 @@ sudo service xrdp start
 apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
 apt-get install -y tightvncserver
 tightvncserver :1
-echo "Download ngrok"
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-read -p "Ctrl + V Authtoken: " CRP 
-./ngrok authtoken $CRP
-./ngrok tcp 3389 &>/dev/null &
-echo RDP Address:
+echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "More  Free VPS akuh.net"
+echo "===================================="
