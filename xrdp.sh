0b09e32dc695c2d11e91a93fcee65bd4f3db7c2f
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-10 19:09:41 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -9,3 +9,11 @@ sudo service xrdp start
 apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
 apt-get install -y tightvncserver
 tightvncserver :1
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP
+./ngrok tcp 3389 &>/dev/null &
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
