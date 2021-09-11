cef1beda017a9cb525d5a55b2ff3f64f2fa28613
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-11 11:29:40 +0700

    Update w7x64gitpod.sh

--- w7x64gitpod.sh
+++ w7x64gitpod.sh
@@ -11,13 +11,11 @@ echo Downloading File From akuh.net
 apt-get install qemu-system-x86 -y
 echo "Wait"
 echo "Starting Windows"
+echo "Wait 1-2 minute to finish bot"
+qemu-system-x86_64 -hda w7x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Ctrl+C To Copy"
-echo "Wait 1-2 minute to finish bot"
-echo "Dont Close This Tab"
-echo "Please support akuh.net thank you"
-qemu-system-x86_64 -hda w7x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
 echo "===================================="
 echo "Keep supporting akuh.net, thank you"
 echo "You Got Root access now"
