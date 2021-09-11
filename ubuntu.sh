c9c6c5b15fe347b56e38bd43eef0e9dbe47fc0d6
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-11 16:51:05 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -11,8 +11,6 @@ sudo apt install xubuntu-desktop -y
 sudo apt-get install -y xrdp
 echo gnome-session >~/.xsession
 sudo service xrdp start
-apt-get install -y tightvncserver
-tightvncserver :1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
