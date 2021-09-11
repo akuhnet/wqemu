caccee44e177ef0f60d2ad3914348a7d4bc8aa41
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-11 11:41:38 +0700

    Update w7x64gitpod.sh

--- w7x64gitpod.sh
+++ w7x64gitpod.sh
@@ -16,6 +16,10 @@ clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
+Username: akuh
+Password: Akuh.Net
+echo "===================================="
+echo "===================================="
 echo "Keep supporting akuh.net, thank you"
 echo "You Got Free RDP now"
 echo "Wait 2 minute to finish bot"
