1ddbcbb512ea327975d7384e3a176995a36bf60c
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-11 17:02:10 +0700

    Create mate.sh

---
+++ mate.sh
@@ -0,0 +1,17 @@
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3389 &>/dev/null &
+echo Downloading File From akuh.net
+sudo apt update && apt upgrade -y
+sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon -y
+sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
+sudo service xrdp start
+clear
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "More  Free VPS akuh.net"
+echo "===================================="
