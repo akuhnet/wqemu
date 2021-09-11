591c7b382a7ac38c90100f004de19648f57d2215
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-11 11:37:59 +0700

    Update w7x64gitpod.sh

--- w7x64gitpod.sh
+++ w7x64gitpod.sh
@@ -11,13 +11,15 @@ echo Downloading File From akuh.net
 apt-get install qemu-system-x86 -y
 echo "Wait"
 echo "Starting Windows"
-echo "Wait 1-2 minute to finish bot"
 qemu-system-x86_64 -hda w7x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
 echo "Keep supporting akuh.net, thank you"
-echo "You Got Root access now"
+echo "You Got Free RDP now"
+echo "Wait 2 minute to finish bot"
+echo "You can close this tab"
+echo "RDP runs for 50 hours"
 echo "===================================="
 sleep 43200
