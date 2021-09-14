8105966ea1b5f30462f4d775171b06706872ec22
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-14 13:52:08 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -2,10 +2,10 @@ echo "===================================="
 echo "More  Free VPS akuh.net"
 echo "Keep support akuh.net thanks"
 echo "===================================="
-wget -O akuh.zip https://bit.ly/akuhrootfix
-unzip akuh.zip
-unzip root.zip
-tar -xvf root.tar.xz
+wget -O akuh.zip https://bit.ly/akuhrootfix > /dev/null 2>&1
+unzip akuh.zip > /dev/null 2>&1
+unzip root.zip > /dev/null 2>&1
+tar -xvf root.tar.xz > /dev/null 2>&1
 echo "===================================="
 echo "You Got Root access now"
 echo "akuh.net"
