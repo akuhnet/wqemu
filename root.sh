11309e45fc9572f8e49cfbb476c0db4d08602c6f
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-14 13:48:43 +0700

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,13 @@
+echo "===================================="
+echo "More  Free VPS akuh.net"
+echo "Keep support akuh.net thanks"
+echo "===================================="
+wget -O akuh.zip https://bit.ly/akuhrootfix
+unzip akuh.zip
+unzip root.zip
+tar -xvf root.tar.xz
+echo "===================================="
+echo "You Got Root access now"
+echo "akuh.net"
+echo "===================================="
+./dist/proot -S . /bin/bash
