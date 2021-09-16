402c349df1b786d4a728a29bcb50485bef8241a4
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-17 01:01:23 +0700

    Update installw10x64.sh

--- installw10x64.sh
+++ installw10x64.sh
@@ -6,15 +6,17 @@ echo "===================================="
 echo "===================================="
 echo "update VPS please wait"
 echo "===================================="
-apt-get update
-apt-get upgrade
-clear
+apt-get update -y > /dev/null 2>&1
+apt-get upgrade -y > /dev/null 2>&1
+echo "===================================="
+echo "===================================="
 echo "Installing Windows"
 echo "===================================="
 apt-get install qemu > /dev/null 2>&1
 echo "===================================="
 echo "===================================="
 echo "Starting Windows"
+echo "===================================="
 echo "===================================="
 nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::3388-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
 echo "===================================="
