fbefb8de774dece8b80892386e95f4b79d92d3b4
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-17 00:06:58 +0700

    Update w10x64kvm.sh

--- w10x64kvm.sh
+++ w10x64kvm.sh
@@ -13,4 +13,3 @@ echo "===================================="
 echo "Starting Windows"
 echo "===================================="
 nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::3388-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
-sleep 43200
