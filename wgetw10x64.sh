cb87772d2479630bf602b3be4ec95553199123a9
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-17 00:07:21 +0700

    Create wgetw10x64.sh

---
+++ wgetw10x64.sh
@@ -0,0 +1,39 @@
+echo "===================================="
+echo "Downloading files akuh.net"
+echo "Get root"
+echo "===================================="
+wget -O su.sh https://raw.githubusercontent.com/akuhnet/wqemu/main/root.sh > /dev/null 2>&1
+echo "===================================="
+echo "Downloading files akuh.net"
+echo "Get windows 10x64"
+echo "===================================="
+wget -O w10x64.img https://bit.ly/akuhnetW10x64 > /dev/null 2>&1
+echo "===================================="
+echo "Downloading files akuh.net"
+echo "Finishing Download"
+echo "===================================="
+wget -O w10x64.sh https://raw.githubusercontent.com/akuhnet/wqemu/main/w10x64kvm.sh > /dev/null 2>&1
+echo "===================================="
+echo "Installing File"
+echo "===================================="
+chmod +x su.sh
+./su.sh
+echo "===================================="
+echo "Installing Windows"
+echo "===================================="
+chmod +x w10x64.sh
+./w10x64.sh
+echo "===================================="
+echo "RDP Address is your ip server"
+echo "===================================="
+echo "===================================="
+echo "Wait 1-2 minute to finish bot"
+echo "===================================="
+echo "Free VPS Windows 10 x64"
+echo "Please support akuh.net thank you"
+echo "Please support akuh.net thank you"
+echo "===================================="
+echo "===================================="
+echo "Username: akuh"
+echo "Password: Akuh.Net"
+echo "===================================="
