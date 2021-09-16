7668ef34edb2c379a8791bc0b86d64eb21c66bff
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-17 00:54:46 +0700

    Update wgetw10x64.sh

--- wgetw10x64.sh
+++ wgetw10x64.sh
@@ -20,7 +20,6 @@ echo "Downloading files akuh.net"
 echo "Get windows Files w10x64"
 echo "===================================="
 curl -L -o w10x64.img https://bit.ly/akuhnetW10x64
-clear
 echo "===================================="
 echo "===================================="
 echo "You Got Root access now"
@@ -28,32 +27,3 @@ echo "akuh.net"
 echo "===================================="
 echo "===================================="
 ./dist/proot -S . /bin/bash
-echo "===================================="
-echo "===================================="
-echo "update VPS please wait"
-echo "===================================="
-apt-get update
-apt-get upgrade
-clear
-echo "Installing Windows"
-echo "===================================="
-apt-get install qemu > /dev/null 2>&1
-echo "===================================="
-echo "===================================="
-echo "Starting Windows"
-echo "===================================="
-nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::3388-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
-echo "===================================="
-echo "RDP Address is your ip server"
-echo "===================================="
-echo "===================================="
-echo "Wait 1-2 minute to finish bot"
-echo "===================================="
-echo "Free VPS Windows 10 x64"
-echo "Please support akuh.net thank you"
-echo "Please support akuh.net thank you"
-echo "===================================="
-echo "===================================="
-echo "Username: akuh"
-echo "Password: Akuh.Net"
-echo "===================================="
