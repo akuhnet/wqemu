15b1b52ea0adec11f8f3afb8f552b89024f5fccd
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-17 07:57:30 +0700

    Update installw10x64.sh

--- installw10x64.sh
+++ installw10x64.sh
@@ -18,12 +18,13 @@ echo "===================================="
 echo "Installing Windows"
 echo "===================================="
 apt-get install qemu -y > /dev/null 2>&1
+apt-get install qemu-system -y > /dev/null 2>&1
 echo "===================================="
 echo "===================================="
 echo "Starting Windows"
 echo "===================================="
 echo "===================================="
-nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::3388-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
+qemu-system-x86_64 -hda w10x64.img -m 4G -smp -enable-kvm -cpu host,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 echo "===================================="
 echo "===================================="
 echo "Wait 1-2 minute to finish bot"
@@ -32,6 +33,7 @@ echo "Free VPS Windows 10 x64"
 echo "Please support akuh.net thank you"
 echo "Please support akuh.net thank you"
 echo "===================================="
+apt install curl -y > /dev/null 2>&1
 echo "===================================="
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
