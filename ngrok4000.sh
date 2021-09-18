3e527527ba460d978c645a3904d4c31320464b65
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-18 11:09:49 +0700

    Delete ngrok4000.sh

--- ngrok4000.sh
+++
@@ -1,6 +0,0 @@
-echo "Download ngrok"
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Ctrl + V Authtoken: " CRP 
-./ngrok authtoken $CRP 
-./ngrok tcp 4000
