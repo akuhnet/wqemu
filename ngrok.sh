71153515007771201ff822743712f6140ef2cadf
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-18 10:56:26 +0700

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,7 @@
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+./ngrok tcp 3388 &>/dev/null &
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
