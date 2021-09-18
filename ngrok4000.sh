a2209126d82aa20cbbb3fdf25d7d9e4b54ce0056
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-18 11:04:40 +0700

    Update ngrok4000.sh

--- ngrok4000.sh
+++ ngrok4000.sh
@@ -4,4 +4,3 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
 ./ngrok tcp 4000 &>/dev/null &
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
