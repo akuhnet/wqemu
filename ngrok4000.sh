2e8b50b9af60b997919d4ea67629dc9dd4539635
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-18 11:07:32 +0700

    Update ngrok4000.sh

--- ngrok4000.sh
+++ ngrok4000.sh
@@ -3,4 +3,4 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/nu
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-./ngrok tcp 4000 &>/dev/null &
+./ngrok tcp 4000
