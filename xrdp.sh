88b6dad10e987c3b99afb235da3366e6817124b6
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-27 07:21:50 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,4 +1,5 @@
-echo -e "$123\n$123\n" | sudo passwd
+echo -e "$123\n$123\n" | sudo adduser akuhnet
+sudo usermod -aG sudo,adm akuhnet
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
 chmod +x ng.sh
@@ -6,13 +7,13 @@ chmod +x ng.sh
 echo "===================================="
 echo "Install XFCE"
 echo "===================================="
-sudo apt install -y xfce4 xfce4-goodies
-sudo apt-get install -y xrdp
+sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+sudo apt-get install -y xrdp > /dev/null 2>&1
 echo xfce4-session >~/.xsession
 sudo service xrdp start
 clear
 echo "===================================="
-echo "Username : root"
+echo "Username : akuhnet"
 echo "Password : 123"
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
