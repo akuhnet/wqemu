d9cfa4a9c219964875293dbb9c23df519560447c
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-27 07:13:34 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,20 +1,19 @@
-rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-echo "Download ngrok"
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Ctrl + V Authtoken: " CRP 
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3389 &>/dev/null &
-echo Downloading File From akuh.net
+echo -e "$123\n$123\n" | sudo passwd
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+echo "===================================="
+echo "Install XFCE"
+echo "===================================="
 sudo apt install -y xfce4 xfce4-goodies
 sudo apt-get install -y xrdp
 echo xfce4-session >~/.xsession
 sudo service xrdp start
-apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
-apt-get install -y tightvncserver
-tightvncserver :1
-echo XRDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+clear
 echo "===================================="
-echo "More  Free VPS akuh.net"
+echo "Username : root"
+echo "Password : 123"
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
