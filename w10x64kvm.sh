ddd4a043bfb07ba917d96295e236be9b23bd81fa
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-27 15:14:43 +0700

    Update w10x64kvm.sh

--- w10x64kvm.sh
+++ w10x64kvm.sh
@@ -4,9 +4,6 @@ echo "===================================="
 wget -O update.sh https://bit.ly/akuhnetupdateall
 chmod +x update.sh
 ./update.sh
-echo "===================================="
-echo "Download ngrok"
-echo "===================================="
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
 chmod +x ng.sh
@@ -23,14 +20,6 @@ echo "===================================="
 apt install qemu -y > /dev/null 2>&1
 apt install qemu-system -y > /dev/null 2>&1
 echo "===================================="
-echo "===================================="
-echo "Starting Windows"
-echo "===================================="
-qemu-system-x86_64 -m 4G -hda w10.qcow2 -boot d -enable-kvm -nographic -net nic -net user,hostfwd=tcp::3388-:3389 -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=4 -vga std -machine type=pc,accel=kvm > /dev/null 2>&1
-echo "===================================="
-echo "Wait 1 minute to finish bot"
-echo "===================================="
-echo "===================================="
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
@@ -38,3 +27,8 @@ echo "===================================="
 echo "Username: administrator"
 echo "Password: akuhnet"
 echo "===================================="
+echo "Starting Windows"
+echo "Wait 1 minute to finish bot"
+echo "===================================="
+qemu-system-x86_64 -m 4G -hda w10.qcow2 -boot d -enable-kvm -nographic -net nic -net user,hostfwd=tcp::3388-:3389 -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=4 -vga std -machine type=pc,accel=kvm > /dev/null 2>&1
+
