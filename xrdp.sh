3c6a3696f0eafc1ceeefcbd327836a091a686327
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-27 07:50:21 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -21,6 +21,7 @@ echo "=======================100%"
 echo "===================================="
 echo "Username : akuhnet"
 echo "Password : 123"
+echo "right click to open menu"
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
