6b29b2d1f3504349f086d1e51f89140945a6a179
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-27 07:23:19 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -8,10 +8,14 @@ echo "===================================="
 echo "Install XFCE"
 echo "===================================="
 sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+echo "=============50%"
 sudo apt-get install -y xrdp > /dev/null 2>&1
+echo "=================70%"
 echo xfce4-session >~/.xsession
+echo "=====================90%"
 sudo service xrdp start
 clear
+echo "=======================100%"
 echo "===================================="
 echo "Username : akuhnet"
 echo "Password : 123"
