6eeb6fe7076cfbde8fab13f537854c7f2cce3786
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-27 15:52:19 +0700

    Update w10x64kvm.sh

--- w10x64kvm.sh
+++ w10x64kvm.sh
@@ -8,11 +8,12 @@ clear
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
 chmod +x ng.sh
-./ng.sh
 clear
+./ng.sh
 echo "===================================="
 echo "Download windows 10x64"
 echo "===================================="
+clear
 curl -L -o w10.qcow2 https://bit.ly/akuhnetw10fix
 clear
 echo "===================================="
@@ -27,15 +28,15 @@ sleep 2
 echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
 echo -ne '\n'
 echo "===================================="
+echo "Starting Windows"
+echo "===================================="
+qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::3388-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=w10.qcow2,index=0,media=disk,format=qcow2,if=virtio -boot once=d &>/dev/null &
+echo "===================================="
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
 echo "===================================="
 echo "Username: administrator"
 echo "Password: akuhnet"
-echo "===================================="
-echo "Starting Windows"
 echo "Wait 1 minute to finish bot"
 echo "===================================="
-qemu-system-x86_64 -m 4G -hda w10.qcow2 -boot d -enable-kvm -nographic -net nic -net user,hostfwd=tcp::3388-:3389 -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=4 -vga std -machine type=pc,accel=kvm > /dev/null 2>&1
-
