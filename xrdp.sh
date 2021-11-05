b526b4ef71ddd4b65cd13260cb2c7aae8afc838c
akuh <81416541+akuhnet@users.noreply.github.com>
2021-11-05 07:48:52 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -3,9 +3,22 @@ echo "akuhnet:123" | sudo chpasswd
 sudo usermod -aG sudo,adm akuhnet
 clear
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3389 &>/dev/null &
 echo "===================================="
 echo "Install XFCE"
 echo "===================================="
