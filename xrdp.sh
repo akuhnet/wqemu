24c2197244c53bc4951d7a4dbda6663455609eb1
akuh <81416541+akuhnet@users.noreply.github.com>
2021-11-24 17:17:56 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,3 +1,4 @@
+apt install sudo
 sudo adduser akuhnet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "akuhnet:123" | sudo chpasswd
 sudo usermod -aG sudo,adm akuhnet
@@ -23,8 +24,11 @@ echo "===================================="
 echo "Install XFCE"
 echo "===================================="
 sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+echo "=======25%"
+sudo apt install firefox -y > /dev/null 2>&1
 echo "=============50%"
 sudo apt-get install -y xrdp > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal
 echo "=================70%"
 echo xfce4-session >~/.xsession
 echo "=====================90%"
