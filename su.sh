2da45a719582a4c454f35b3759c183da5b9f5b30
akuh <81416541+akuhnet@users.noreply.github.com>
2022-04-05 12:21:02 +0700

    Update su.sh

--- su.sh
+++ su.sh
@@ -1,13 +1,16 @@
 echo "===================================="
 echo "More  Free VPS akuh.net"
+echo "Keep support akuh.net thanks"
+echo "Please Wait"
 echo "===================================="
-pip install googledrivedownloader
-wget https://raw.githubusercontent.com/akuhnet/wqemu/main/getrot.py
-python getrot.py
+wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
+unzip akuh.zip
 unzip root.zip
 tar -xvf root.tar.xz
+rm -rf akuh.zip root.zip root.tar.xz
+clear
 echo "===================================="
-echo "Keep supporting akuh.net, thank you"
 echo "You Got Root access now"
+echo "akuh.net"
 echo "===================================="
 ./dist/proot -S . /bin/bash
