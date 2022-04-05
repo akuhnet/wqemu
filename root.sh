8fde9c24f88b973a34e4ada17ab29d27fabd3b63
akuh <81416541+akuhnet@users.noreply.github.com>
2022-04-05 12:16:45 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -7,6 +7,7 @@ wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/
 unzip akuh.zip > /dev/null 2>&1
 unzip root.zip > /dev/null 2>&1
 tar -xvf root.tar.xz > /dev/null 2>&1
+rm -rf akuh.zip root.zip
 echo "===================================="
 echo "You Got Root access now"
 echo "akuh.net"
