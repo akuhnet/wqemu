78d13c7d29985a427e23c3baeefa426ba3c71828
akuh <81416541+akuhnet@users.noreply.github.com>
2022-04-05 12:18:24 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -3,11 +3,12 @@ echo "More  Free VPS akuh.net"
 echo "Keep support akuh.net thanks"
 echo "Please Wait"
 echo "===================================="
-wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip > /dev/null 2>&1
-unzip akuh.zip > /dev/null 2>&1
-unzip root.zip > /dev/null 2>&1
-tar -xvf root.tar.xz > /dev/null 2>&1
-rm -rf akuh.zip root.zip
+wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
+unzip akuh.zip
+unzip root.zip
+tar -xvf root.tar.xz
+rm -rf akuh.zip root.zip root.tar.xz
+clear
 echo "===================================="
 echo "You Got Root access now"
 echo "akuh.net"
