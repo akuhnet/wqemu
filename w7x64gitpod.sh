2c5a9b3da4cff2751eea4310a048cc062543a0e3
egonujol <136145810+egonujol@users.noreply.github.com>
2023-06-10 05:41:16 +0000

    Update w7x64gitpod.sh

--- w7x64gitpod.sh
+++ w7x64gitpod.sh
@@ -1,6 +1,6 @@
 apt-get update
 echo "Download windows files"
-wget -O w7x64.img https://bit.ly/akuhnetw7X64
+wget -O w7x86.img https://bit.ly/akuhnetw7x86
 echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
@@ -11,7 +11,7 @@ echo Downloading File From akuh.net
 apt-get install qemu-system-x86 -y
 echo "Wait"
 echo "Starting Windows"
-qemu-system-x86_64 -hda w7x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda w7x86.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
